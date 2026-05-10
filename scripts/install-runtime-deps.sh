92e17076a0f9c979b5cee36a03b49926043d8cb0
Qing Dong <sweetreg@126.com>
2026-05-10 21:11:44 +0800

    chore: install runtime deps as current user

--- scripts/install-runtime-deps.sh
+++ scripts/install-runtime-deps.sh
@@ -9,9 +9,14 @@ else
   docker_cmd=(sudo docker)
 fi
 
+user_args=()
+if [ "$(id -u)" != "0" ]; then
+  user_args=(-u "$(id -u):$(id -g)" -e HOME=/tmp)
+fi
+
 "${docker_cmd[@]}" run --rm \
+  "${user_args[@]}" \
   -v "$PWD:/app" \
   -w /app \
   node:22-alpine \
   npm ci --omit=dev
-
